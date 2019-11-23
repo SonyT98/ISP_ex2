3125e58bb0ebe7f2f43ef35085c6f21c2380dcd5
SonyT98 <57562876+SonyT98@users.noreply.github.com>
2019-11-23 18:14:35 +0200

    added testgradefunction declerations

--- Ex2/TestGrade/HardCodedData.h
+++ Ex2/TestGrade/HardCodedData.h
@@ -4,5 +4,18 @@
 
 #define NUM_THREADS 2
 #define ERROR_CODE ((int)(-1))
+#define EX1 0
+#define EX2 1
+#define EX3 2
+#define EX4 3
+#define EX5 4
+#define EX6 5
+#define EX7 6
+#define EX8 7
+#define EX9 8
+#define EX10 9
+#define QUIZ 10
+#define EXAM_A 11
+#define EXAM_B 12
 
 #endif // __HARDCODEDDATA_H___
---
+++ Ex2/TestGrade/TestGradeFunctions.h
@@ -0,0 +1,45 @@
+#pragma once
+#ifndef __TESTGRADEFUNCTIONS_H__
+#define __TESTGRADEFUNCTIONS_H___
+
+// Includes --------------------------------------------------------------------
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <windows.h>
+#include <limits.h>
+#include <string.h>
+
+// Function Declarations -------------------------------------------------------
+
+/*
+* create threads to all grades possible for student, and fill the grade array.
+* Input Arguments:
+*	folder_path: string name of the full/relative folder path of the specific student.
+*	grades_array: empty array of grades of the student. the function will fill the 
+*	grades_array with the correct grades from the file.
+* Return:
+*	returns -1 if there was an error. 
+*/
+int ReadAllGrades(char *folder_path, int* grades_array);
+
+/*
+* calculate the final grade.
+* Input Arguments:
+*	grades_array: the array of grades of the student.
+* Return:
+*	returns the final grade.
+*/
+int CalculateFinalGrade(int* grades_array);
+
+/*
+* Print the final grade to the txt file of the student
+* Input Arguments:
+*	folder_path: string name of the full/relative folder path of the specific student.
+*	final_grade: the final grade of the student.
+* Return:
+*	returns -1 if the file operations fail.
+*/
+int PrintFinalGradeToFile(char *folder_path, int final_grade);
+
+#endif // __HARDCODEDDATA_H___
