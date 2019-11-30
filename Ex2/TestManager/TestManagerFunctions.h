a6dc9a6b772a56bb44dbb89d064f7821cb5a0be8
nadavnave <57562882+nadavnave@users.noreply.github.com>
2019-11-30 16:07:07 +0200

    added function declarations and doc

--- Ex2/TestManager/TestManagerFunctions.h
+++ Ex2/TestManager/TestManagerFunctions.h
@@ -16,4 +16,44 @@
 
 // Function Declarations -------------------------------------------------------
 
+
+/*
+* getIdsFromFile extract all ids from the file studentIds.txt into an array of string.
+* Input Arguments:
+*	path	- Full path to the main folder containing studentIds.txt
+*	ids		- array of string to be filled by the function with the student id's.
+* Return:
+*	the number of students in the list. return -1 if failed.
+*/
+
+int GetIdsFromFile(char *path, char **ids);
+
+/*
+* getStudentsFinalGrades gets all students final grades by calling the TestGrade process.
+* Input Arguments:
+*	path					- Full path to the main folder containing studentIds.txt
+*	ids						- array of string containing students ids.
+*	number_of_students		- the number of students in ids array.
+*	grades					- array of integers to be filled with the students grades.
+* Return:
+*	 return -1 if failed.
+*/
+
+int GetStudentsFinalGrades(char *path, char **ids,int number_of_students, int **grades);
+
+
+/*
+* WriteFinalGradesFile gets all students final grades by calling the TestGrade process.
+* Input Arguments:
+*	path					- Full path to the main folder containing studentIds.txt
+*	ids						- array of string containing students ids.
+*	number_of_students		- the number of students in ids array.
+*	grades					- array of integers to be filled with the students grades.
+* Return:
+*	 return -1 if failed.
+*/
+
+
+int WriteFinalGradesFile(char **ids, int number_of_students, int **grades);
+
 #endif // __TESTMANAGERFUNCTIONS_H___
